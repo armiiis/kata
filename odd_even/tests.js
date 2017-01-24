c12a36b1e6153d756adea20cd7796fd99f2efa23
armiiis <musteikis.arminas@gmail.com>
2017-01-24 11:53:20 +0200

    odd_even

---
+++ odd_even/tests.js
@@ -0,0 +1,10 @@
+function even_or_odd(number) {
+    return number % 2 === 0 ? "Even" : "Odd";
+}
+QUnit.test("hello test", function (assert) {
+    assert.equal(even_or_odd(-2), "Even");
+    assert.equal(even_or_odd(2), "Even");
+    assert.equal(even_or_odd(0), "Even");
+    assert.equal(even_or_odd(7), "Odd");
+    assert.equal(even_or_odd(1), "Odd");
+});
